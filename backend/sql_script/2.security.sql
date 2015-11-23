db3e03259faf8d400f8749d88f782f8518c61548
sarawuti <sarawut.inthong@allianz.com>
2015-11-23 16:45:46 +0700

    update

--- backend/sql_script/2.security.sql
+++ backend/sql_script/2.security.sql
@@ -3,7 +3,7 @@ CREATE TABLE roles ( id int NOT NULL AUTO_INCREMENT, username varchar(45), autho
 insert into roles (id, username, authority, name) values (1, 'admin', 'ROLE_ADMIN', 'Admin');
 DROP TABLE users;
 CREATE TABLE users ( id int NOT NULL AUTO_INCREMENT, username_show varchar(45) NOT NULL, username varchar(45) NOT NULL, password varchar(255) NOT NULL, enabled tinyint DEFAULT '1' NOT NULL, created_date_time datetime, updated_date_time datetime, PRIMARY KEY (id), CONSTRAINT ix1 UNIQUE (username) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-insert into users (id, username, password, enabled, created_date_time, updated_date_time) values (1, 'Admin', 'admin', '$2a$10$mNHt9RDl.3/ifSOCU44EWeP7lIeMqHy.mYObhX4DRBayPiNvpJU16', 1, null, null);
+insert into users (id, username_show, username, password, enabled, created_date_time, updated_date_time) values (1, 'Admin', 'admin', '$2a$10$mNHt9RDl.3/ifSOCU44EWeP7lIeMqHy.mYObhX4DRBayPiNvpJU16', 1, null, null);
 
 
 CREATE TABLE `hibernate_sequences` (
