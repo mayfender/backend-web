f1f587015a7ae25f5aa7b874fdb4aba0e81f4ebc
sarawuti <sarawut.inthong@allianz.com>
2015-11-26 11:57:56 +0700

    update

--- backend/src/main/java/com/may/ple/backend/JerseyConfig.java
+++ backend/src/main/java/com/may/ple/backend/JerseyConfig.java
@@ -1,11 +1,14 @@
 package com.may.ple.backend;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.ext.ContextResolver;
 
 import org.apache.log4j.Logger;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.may.ple.backend.action.UserAction;
 
 @Component
@@ -15,7 +18,23 @@ public class JerseyConfig extends ResourceConfig {
 	
 	public JerseyConfig() {
 		LOG.info(":----------: Register Rest Service :----------:");
+		register(new ObjectMapperContextResolver());
 		register(UserAction.class);
 	}
 
+}
+
+class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
+    private final ObjectMapper mapper;
+
+    public ObjectMapperContextResolver() {
+        mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(Include.NON_NULL);
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+        return mapper;
+    }
+    
 }
