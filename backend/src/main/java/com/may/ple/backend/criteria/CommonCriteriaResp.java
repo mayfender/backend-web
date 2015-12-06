eb0afca6dcf486b76cce96e04cfa55c2313df015
mayfender <mayfender@mayfender-PC>
2015-12-06 11:52:20 +0700

    add toString

--- backend/src/main/java/com/may/ple/backend/criteria/CommonCriteriaResp.java
+++ backend/src/main/java/com/may/ple/backend/criteria/CommonCriteriaResp.java
@@ -1,9 +1,17 @@
 package com.may.ple.backend.criteria;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public abstract class CommonCriteriaResp {
 	private int statusCode = 9999;
 	
 	public CommonCriteriaResp(){}
+	
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.DEFAULT_STYLE);
+	}
 	
 	public CommonCriteriaResp(int statusCode) {
 		this.statusCode = statusCode;
