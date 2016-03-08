338028fda1d048f7fda4274d2b31a1b5c65db5b2
sarawuti <sarawut.inthong@allianz.com>
2016-03-08 14:20:58 +0700

    update

--- backend/src/main/webapp/app/scripts/controllers/position/positionCtrl.js
+++ backend/src/main/webapp/app/scripts/controllers/position/positionCtrl.js
@@ -1,5 +1,9 @@
 angular.module('sbAdminApp').controller('PositionCtrl', function($rootScope, $scope, $base64, $http, $translate, urlPrefix) {
 	
-	console.log('testing PositionCtrl');
+	$scope.users = [
+	                {id: 1, name: 'ผู้จัดการ', status: 0},
+	                {id: 2, name: 'Admin', status: 0},
+	                {id: 3, name: 'ผู้ช่วย', status: 1}
+	              ]; 
 	
 });
