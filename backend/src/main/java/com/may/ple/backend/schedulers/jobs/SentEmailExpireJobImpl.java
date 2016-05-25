6e0f036453c7f5a351fb61cd2231e6960c4c82c8
mayfender <mayfender@mypc>
2016-05-25 08:01:30 +0700

    update

--- backend/src/main/java/com/may/ple/backend/schedulers/jobs/SentEmailExpireJobImpl.java
+++ backend/src/main/java/com/may/ple/backend/schedulers/jobs/SentEmailExpireJobImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.mail.internet.MimeMessage;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -48,62 +49,96 @@ public class SentEmailExpireJobImpl implements Job {
 			
 			try {
 				
-				LOG.debug("Find 60 days advance.");
+				LOG.debug("Notify 60 days advance.");
 				List<SptRegistration> expireAdvance = sptRegistrationService.findExpireAdvance(60);
-				MimeMessage mail;
-				MimeMessageHelper helper;
+				String subj = "แจ้งต่ออายุสมาชิก";
+				StringBuilder body = new StringBuilder();
+				body.append("เรียน คุณxxx yyy\n\n");
+				body.append("บริษัทฯ รู้สึกเป็นเกียรติ\n");
+				body.append("และมีความยินดีอย่างยิ่งที่ได้รับความไว้วางใจจากท่านสมัครเป็นสมาชิกซุปเปอร์เทรดเดอร์ รีพับบลิค\n");
+				body.append("ด้วยความตั้งใจของทีมงานที่จะสร้างให้เกิดเป็นพื้นที่ในการเรียนรู้การลงทุนอย่างถูกต้อง\n");
+				body.append("สร้างผลตอบแทนจากการลงทุนที่เหมาะสมและยั่งยืน\n");
+				body.append("ทั้งนี้บริษัทฯ ใคร่ขอแจ้งต่อท่านสมาชิกว่า อายุสมาชิกของท่านเหลือ zzz เดือน\n");
+				body.append("โดยสิทธิสมาชิกของท่านจะสิ้นสุดในวันที่ ttt หากท่านมีความประสงค์ต้องการต่ออายุสมาชิก\n");
+				body.append("สามารถติดต่อกับเจ้าหน้าที่ได้ที่ ซุปเปอร์เทรดเดอร์ รีพับบลิค อาคารเอ็มไพร์ทาวเวอร์ ชั้นที่ 19 ตึก 3\n\n");
+				body.append("ด้วยความเคารพ\n\n");
+				body.append("ทีมงานซุปเปอร์เทรดเดอร์ รีพับบลิค\n\n");
 				
-				StringBuilder msg = new StringBuilder();
-				msg.append("เรียน คุณxxx xxxx\n\n");
-				msg.append("บริษัทฯ รู้สึกเป็นเกียรติ\n");
-				msg.append("และมีความยินดีอย่างยิ่งที่ได้รับความไว้วางใจจากท่านสมัครเป็นสมาชิกซุปเปอร์เทรดเดอร์ รีพับบลิค\n");
-				msg.append("ด้วยความตั้งใจของทีมงานที่จะสร้างให้เกิดเป็นพื้นที่ในการเรียนรู้การลงทุนอย่างถูก\n");
-				msg.append("ต้อง สร้างผลตอบแทนจากการลงทุนที่เหมาะสมและยั่งยืน\n");
-				msg.append("ทั้งนี้บริษัทฯ ใคร่ขอแจ้งต่อท่านสมาชิกว่า สิทธิสมาชิกของท่านได้หมดแล้ว\n");
-				msg.append("หากท่านมีความประสงค์ต้องการต่ออายุสมาชิก สามารถติดต่อกับเจ้าหน้าที่ได้ที่\n");
-				msg.append("ซุปเปอร์เทรดเดอร์ รีพับบลิค อาคารเอ็มไพร์ทาวเวอร์ ชั้นที่ 19 ตึก 3\n\n");
-				msg.append("ด้วยความเคารพ\n\n");
-				msg.append("ทีมงานซุปเปอร์เทรดเดอร์ รีพับบลิค\n\n");
+				String bodyStr;
 				
 				for (SptRegistration reg : expireAdvance) {
-					mail = mailSender.createMimeMessage();
-					helper = new MimeMessageHelper(mail, true);
-					helper.setTo(reg.getConEmail());
-					helper.setSubject("แจ้งการหมดอายุสมาชิก");
-					helper.setText(msg.toString().replaceFirst("xxx", reg.getFirstname()).replaceFirst("xxxx", reg.getLastname()));
+					bodyStr = body.toString()
+							  .replaceFirst("xxx", reg.getFirstname())
+						      .replaceFirst("yyy", reg.getLastname())
+						      .replaceFirst("zzz", "2")
+						      .replaceFirst("ttt", String.format("%1$td/%1$tm/%1$tY", reg.getExpireDate()));
 					
-					mailSender.send(mail);
+					if(StringUtils.isBlank(reg.getConEmail())) continue;
+					
+					sendMail(reg.getConEmail(), subj, bodyStr);
 				}
 				
+				//---------------------------------------------------------------------
 				
-				
-				LOG.debug("Find 30 days advance.");
+				LOG.debug("Notify 30 days advance.");
 				expireAdvance = sptRegistrationService.findExpireAdvance(30);
+				
 				for (SptRegistration reg : expireAdvance) {
-					LOG.debug(reg);					
+					bodyStr = body.toString()
+					  .replaceFirst("xxx", reg.getFirstname())
+				      .replaceFirst("yyy", reg.getLastname())
+				      .replaceFirst("zzz", "1")
+				      .replaceFirst("ttt", String.format("%1$td/%1$tm/%1$tY", reg.getExpireDate()));
+					
+					if(StringUtils.isBlank(reg.getConEmail())) continue;
+					
+					sendMail(reg.getConEmail(), subj, bodyStr);				
 				}
 				
-				LOG.debug("today days advance.");
+				//---------------------------------------------------------------------
+				
+				LOG.debug("today notify.");
 				expireAdvance = sptRegistrationService.findExpireAdvance(0);
+				subj = "แจ้งการหมดอายุสมาชิก";
+				body = new StringBuilder();
+				body.append("เรียน คุณxxx yyy\n\n");
+				body.append("บริษัทฯ รู้สึกเป็นเกียรติ\n");
+				body.append("และมีความยินดีอย่างยิ่งที่ได้รับความไว้วางใจจากท่านสมัครเป็นสมาชิกซุปเปอร์เทรดเดอร์ รีพับบลิค\n");
+				body.append("ด้วยความตั้งใจของทีมงานที่จะสร้างให้เกิดเป็นพื้นที่ในการเรียนรู้การลงทุนอย่างถูกต้อง\n");
+				body.append("สร้างผลตอบแทนจากการลงทุนที่เหมาะสมและยั่งยืน\n");
+				body.append("ทั้งนี้บริษัทฯ ใคร่ขอแจ้งต่อท่านสมาชิกว่า สิทธิสมาชิกของท่านได้หมดแล้ว\n");
+				body.append("หากท่านมีความประสงค์ต้องการต่ออายุสมาชิก สามารถติดต่อกับเจ้าหน้าที่ได้ที่\n");
+				body.append("ซุปเปอร์เทรดเดอร์ รีพับบลิค อาคารเอ็มไพร์ทาวเวอร์ ชั้นที่ 19 ตึก 3\n\n");
+				body.append("ด้วยความเคารพ\n\n");
+				body.append("ทีมงานซุปเปอร์เทรดเดอร์ รีพับบลิค\n\n");
+				
 				for (SptRegistration reg : expireAdvance) {
-					LOG.debug(reg);					
+					bodyStr = body.toString().replaceFirst("xxx", reg.getFirstname()).replaceFirst("yyy", reg.getLastname());
+							
+					if(StringUtils.isBlank(reg.getConEmail())) continue;
+							
+					sendMail(reg.getConEmail(), subj, bodyStr);	
 				}
-				
-				
-				/*MimeMessage mail = mailSender.createMimeMessage();
-				MimeMessageHelper helper = new MimeMessageHelper(mail, true);
-		        helper.setTo("mayfender@gmail.com");*/
-		        //helper.setReplyTo("someone@localhost");
-//		        helper.setFrom("mayfender.work@gmail.com");
-		        /*helper.setSubject("Lorem ipsum");
-		        helper.setText("Lorem ipsum dolor sit amet [...]");
-		        
-		        mailSender.send(mail);*/
 				
 			} catch (Exception e) {
 				LOG.error(e.toString(), e);
 			}
 			LOG.debug("End");
+		}
+		
+		public void sendMail(String mailTo, String subj, String body) {
+			try {
+				
+				MimeMessage mail = mailSender.createMimeMessage();
+				MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+				helper.setTo(mailTo);
+				helper.setSubject(subj);
+				helper.setText(body);
+				mailSender.send(mail);
+				
+			} catch (Exception e) {
+				LOG.error(e.toString());
+			}
 		}
 		
 	}
