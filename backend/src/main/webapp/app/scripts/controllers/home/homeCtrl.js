d9f1ff283ddf4d7d2c97c5b9aa03fcf1e99bcd5d
mayfender <mayfender@mypc>
2016-08-07 20:27:35 +0700

    update

--- backend/src/main/webapp/app/scripts/controllers/home/homeCtrl.js
+++ backend/src/main/webapp/app/scripts/controllers/home/homeCtrl.js
@@ -1,4 +1,16 @@
 angular.module('sbAdminApp').controller('HomeCtrl', function($rootScope, $scope, $base64, $http, $translate, $localStorage, $state, urlPrefix) {
+	var auth = new String(angular.copy($rootScope.authority));
+	console.log(auth);
+	console.log($rootScope.authority);
 	
+	if($rootScope.authority == 'ROLE_SUPERVISOR') {
+		$scope.position = 'หัวหน้างาน คอลเลคเตอร์';
+	} else if ($rootScope.authority == 'ROLE_USER') {
+		$scope.position = 'คอลเลคเตอร์';		
+	} else if ($rootScope.authority == 'ROLE_ADMIN') {
+		$scope.position = 'แอดมิน';				
+	} else if ($rootScope.authority == 'ROLE_MANAGER') {
+		$scope.position = 'ผู้จัดการ';				
+	}
 	
 });
