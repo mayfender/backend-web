856218065c3ab2ef083c18f94aec849ca3a3568d
sarawuti <sarawut.inthong@allianz.com>
2016-09-23 17:43:15 +0700

    update

--- backend/src/main/webapp/app/scripts/directives/header/header.js
+++ backend/src/main/webapp/app/scripts/directives/header/header.js
@@ -59,8 +59,12 @@ angular.module('sbAdminApp')
 	        	}
 	        	
 	        	
-	        	
-	        	
+	        	//----------------------: FlipClock :---------------------------------
+	        	var clock = $('.clock').FlipClock(3600 * 24 * 3, {
+	        		clockFace: 'DailyCounter',
+	        		countdown: true
+	        	});
+	        	//----------------------: FlipClock :---------------------------------
 	        	
 	        	
 	        	
