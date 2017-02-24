ba809c09dc3e3e95ad25bce53fb3f09d892b59c7
sarawuti <sarawut.inthong@allianz.com>
2017-02-24 10:15:01 +0700

    update

--- backend/src/main/java/com/may/ple/backend/service/TraceWorkService.java
+++ backend/src/main/java/com/may/ple/backend/service/TraceWorkService.java
@@ -176,7 +176,6 @@ public class TraceWorkService {
 				traceWork.setTemplateId(req.getTemplateId() == null ? null: new ObjectId(req.getTemplateId()));
 				traceWork.setAddressNotice(req.getAddressNotice());
 				traceWork.setAddressNoticeStr(req.getAddressNoticeStr());
-				traceWork.setIsHold(false);
 				
 				Update update = new Update();
 				update.set(SYS_TRACE_DATE.getName(), date);
