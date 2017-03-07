4ae117e31b8a24c63824e84342e6eb0f02b0091d
mayfender <mayfender@mypc>
2017-03-08 06:31:12 +0700

    update

--- backend/src/main/java/com/may/ple/backend/service/ToolsService.java
+++ backend/src/main/java/com/may/ple/backend/service/ToolsService.java
@@ -3,10 +3,15 @@ package com.may.ple.backend.service;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
@@ -17,9 +22,10 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.springframework.stereotype.Service;
 
+import com.may.ple.backend.entity.ColumnFormat;
 import com.may.ple.backend.exception.CustomerException;
 import com.may.ple.backend.model.FileDetail;
-import com.may.ple.backend.utils.POIExcelUtil;
+import com.may.ple.backend.utils.GetAccountListHeaderUtil;
 
 @Service
 public class ToolsService {
@@ -27,29 +33,41 @@ public class ToolsService {
 	
 	public ByteArrayOutputStream excel2txt(InputStream uploadedInputStream, FormDataContentDisposition fileDetail, FileDetail fd) throws Exception {
 		OutputStreamWriter writer = null;
+		Workbook workbook = null;
 		
 		try {
-			LOG.debug("File ext: " + fd.fileExt);
-			if(!fd.fileExt.equals(".xlsx")) throw new CustomerException(5000, "File Type not support");
+			LOG.debug("File ext: " + fd.fileExt);						
 			
-			Workbook workbook = new XSSFWorkbook(uploadedInputStream);
+			if(fd.fileExt.equals(".xlsx")) {
+				workbook = new XSSFWorkbook(uploadedInputStream);
+			} else if(fd.fileExt.equals(".xls")) {
+				workbook = new HSSFWorkbook(uploadedInputStream);
+			} else {
+				throw new CustomerException(5000, "Filetype not match");
+			}
 			
 			Sheet sheet = workbook.getSheetAt(0);
-			POIExcelUtil.removeSheetExcept0(workbook);
+			List<ColumnFormat> columnFormats = new ArrayList<>();
+			Map<String, Integer> headerIndex = GetAccountListHeaderUtil.getFileHeader(sheet, columnFormats);
+			if(headerIndex.size() == 0) {		
+				LOG.error("Not found Headers");
+				return null;
+			}
 			
 			ByteArrayOutputStream outputArray = new ByteArrayOutputStream();
 			writer = new OutputStreamWriter(outputArray);
 			StringBuilder txtRaw = new StringBuilder();
-			int cellIndex;
-			int countNull;
+			Set<String> keySet = headerIndex.keySet();
 			Cell cell;
-			int r = 0;
+			int r = 1;
 			Row row;
 			
 			while(true) {
-				if(r > 0) {
-					txtRaw.deleteCharAt(txtRaw.length() - 1);
-					txtRaw.append(System.getProperty("line.separator"));
+				if(r > 1) {
+					if(txtRaw.length() != 0) {
+						txtRaw.deleteCharAt(txtRaw.length() - 1);
+						txtRaw.append(System.getProperty("line.separator"));
+					}
 				}
 				
 				row = sheet.getRow(r++);
@@ -58,20 +76,12 @@ public class ToolsService {
 					break;
 				}
 				
-				cellIndex = 0;
-				countNull = 0;
-				
-				while(true) {
-					cell = row.getCell(cellIndex++, MissingCellPolicy.RETURN_BLANK_AS_NULL);				
-					
-					if(countNull == 10) break;
+				for (String key : keySet) {
+					cell = row.getCell(headerIndex.get(key), MissingCellPolicy.RETURN_BLANK_AS_NULL);
 					
 					if(cell == null || StringUtils.isBlank(String.valueOf(cell))) {
-						countNull++;
-						continue;
-					} else {
-						countNull = 0;
-						
+						txtRaw.append("|");
+					} else {						
 						txtRaw.append(new DataFormatter(Locale.ENGLISH).formatCellValue(cell) + "|");
 					}
 				}			
@@ -84,6 +94,7 @@ public class ToolsService {
 			LOG.error(e.toString());
 			throw e;
 		} finally {
+			if(workbook != null) workbook.close();
 			if(writer != null) writer.close();
 		}
 	}
