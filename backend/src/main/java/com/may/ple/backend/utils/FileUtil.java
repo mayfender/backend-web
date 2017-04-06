e7f69179db7b019b1bece6897eefe79747eb7f4b
mayfender <mayfender@mypc>
2017-04-07 05:27:33 +0700

    update

--- backend/src/main/java/com/may/ple/backend/utils/FileUtil.java
+++ backend/src/main/java/com/may/ple/backend/utils/FileUtil.java
@@ -17,7 +17,7 @@ public class FileUtil {
 			LOG.debug("Start");
 			int indexFile = fileDetail.getFileName().lastIndexOf(".");
 			String fileName = new String(fileDetail.getFileName().substring(0, indexFile).getBytes("iso-8859-1"), "UTF-8");
-			String fileExt = fileDetail.getFileName().substring(indexFile);
+			String fileExt = fileDetail.getFileName().substring(indexFile).toLowerCase();
 			
 			if(date != null) {
 				fileName = fileName + "_" + String.format(Locale.ENGLISH, "%1$tY%1$tm%1$td%1$tH%1$tM%1$tS%1$tL", date) + fileExt;				
