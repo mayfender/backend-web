23f0a4ca4c830769e70922265771a348c1ba4643
mayfender <mayfender@mypc>
2017-06-03 23:36:48 +0700

    update

--- backend/src/main/webapp/app/scripts/controllers/payment_to_us/paymentToUsCtrl.js
+++ backend/src/main/webapp/app/scripts/controllers/payment_to_us/paymentToUsCtrl.js
@@ -1,5 +1,6 @@
 angular.module('sbAdminApp').controller('PaymentToUsCtrl', function($rootScope, $scope, $base64, $http, $translate, $localStorage, $state, urlPrefix) {
 	
-	console.log('Payment to us');
+	console.log('Payment to us ');
+	$scope.bankIcon = urlPrefix + '/app/images/scb.png';
 
 });
