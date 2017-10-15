8ea223fa794cee3082e1914e2fe988b9f3321117
mayfender <mayfender@mypc>
2017-10-15 19:28:38 +0700

    update

--- backend/src/main/java/com/may/ple/backend/utils/EStudenLoanUtil.java
+++ backend/src/main/java/com/may/ple/backend/utils/EStudenLoanUtil.java
@@ -3,16 +3,17 @@ package com.may.ple.backend.utils;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Map;
 
 import javax.imageio.ImageIO;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Connection.Method;
 import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import com.anti_captcha.Api.ImageToText;
@@ -23,28 +24,48 @@ public class EStudenLoanUtil {
 	private static String captchaImgFilePath = "D:/DMS_DATA/upload/captcha/Captcha.jpg";
 	
 	public static void main(String[] args) {
-		FileInputStream imgInputstream = null;
-		
+		try {
+			String sessionId = login();
+			System.out.println("sessionId : " + sessionId);
+			
+			if(StringUtils.isNoneBlank(sessionId)) {				
+				getPaymentInfo(sessionId);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static String login() throws Exception {
 		try {
 			//[1]
-			String sessionId = loginPage();
+			String sessionId = getLoginPage();
 			
-			//[2]
+			//[2] 
+			String captcha = getCaptchaText();
+			
+			//[3]
+			if(doLogin(sessionId, captcha)) {
+				return sessionId;				
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	private static String getCaptchaText() throws Exception {
+		try {
 			String txt = "";
 			if("local".equals("local")) {				
 				txt = tesseract();
 			} else {
 				txt = antiCaptcha();				
 			}
-			
-			//[3]
-			doLogin(sessionId, txt);
+			return txt;
 		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if(imgInputstream != null) imgInputstream.close();				
-			} catch (Exception e2) {}
+			throw e;
 		}
 	}
 	
@@ -113,7 +134,7 @@ public class EStudenLoanUtil {
 		}
     }
 	
-	private static String loginPage() throws Exception {
+	private static String getLoginPage() throws Exception {
 		try {
 			Response res = Jsoup.connect(link + "/STUDENT/ESLLogin.do").method(Method.GET).execute();
 			Map<String, String> cookie = res.cookies();
@@ -140,7 +161,7 @@ public class EStudenLoanUtil {
 		}
 	}
 	
-	private static void doLogin(String sessionId, String captcha) throws Exception {
+	private static boolean doLogin(String sessionId, String captcha) throws Exception {
 		try {
 			Response res = Jsoup.connect(link + "/STUDENT/ESLLogin.do")
 						.method(Method.POST)
@@ -153,14 +174,58 @@ public class EStudenLoanUtil {
 					   .postDataCharset("UTF-8")
 					   .execute();
 			
-			Document document = res.parse();
-			Elements cusName = document.select("td input[name='stuFullName']");
+			Document doc = res.parse();
+			Elements cusName = doc.select("td input[name='stuFullName']");
+			boolean status = false;
 			
-			if(cusName != null) {				
+			if(cusName != null && StringUtils.isNoneBlank(cusName.val())) {				
 				System.out.println("Login success : " + cusName.val());
-			} else {
+				status = true;
+			} else if(doc.select("#capId") != null) {				
 				System.out.println("Login fail");
+			} else {
+				System.out.println("Service unavailable");
+				System.out.println(doc);
 			}			
+			
+			return status;
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	private static void getPaymentInfo(String sessionId) throws Exception {
+		try {
+			Response res = Jsoup.connect(link + "/STUDENT/ESLMTI001.do")
+						.method(Method.POST)
+					   .data("loanType", "F101")
+					   .data("accNo", "1006277854")
+					   .data("cif", "")
+					   .data("browser", "Fire Fox Or Other")
+					   .header("Content-Type", "application/x-www-form-urlencoded")
+					   .cookie("JSESSIONID", sessionId)
+					   .postDataCharset("UTF-8")
+					   .execute();
+			
+			Document doc = res.parse();
+			Elements table = doc.select("#tab4 table table");
+			Elements rows = table.select("tr");
+			Elements cols;
+			boolean isFirstRow = true;
+			
+			for (Element row : rows) {
+				cols = row.select("td");
+				
+				for (Element col : cols) {
+					if(isFirstRow) {
+						System.out.print(String.format("%25s", col.select("div").text() + "| "));
+						isFirstRow = false;
+					} else {
+						System.out.print(String.format("%25s", col.text() + "| "));												
+					}
+				}
+				System.out.println();
+			}
 		} catch (Exception e) {
 			throw e;
 		}
