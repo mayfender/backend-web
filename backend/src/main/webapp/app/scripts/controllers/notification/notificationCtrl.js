7f75ff1c68d9911e2fc4ff2769bdc68699551441
sarawuti <sarawut.inthong@allianz.com>
2018-06-21 17:00:15 +0700

    update

--- backend/src/main/webapp/app/scripts/controllers/notification/notificationCtrl.js
+++ backend/src/main/webapp/app/scripts/controllers/notification/notificationCtrl.js
@@ -19,17 +19,21 @@ angular.module('sbAdminApp').controller('NotificationCtrl', function($rootScope,
 		minTime: '07:00',
 		maxTime: '21:00'
 	};
-		
-	$scope.notificationList = [{id: '', name: 'New Comment', dateTimeDesc: '4 minutes ago', isTakeAction: true}, 
-	                           {id: '', name: 'Message Sent', dateTimeDesc: '12 minutes ago', isTakeAction: false}, 
-	                           {id: '', name: 'New Task', dateTimeDesc: '27 minutes ago', isTakeAction: true}, 
-	                           {id: '', name: 'New Followers', dateTimeDesc: '43 minutes ago', isTakeAction: true}, 
-	                           {id: '', name: 'Server Rebooted', dateTimeDesc: '11:32 AM', isTakeAction: false}, 
-	                           {id: '', name: 'Server Crashed!', dateTimeDesc: '11:13 AM', isTakeAction: false}, 
-	                           {id: '', name: 'Server Not Responding', dateTimeDesc: '10:57 AM', isTakeAction: true}, 
-	                           {id: '', name: 'New Order Placed', dateTimeDesc: '9:49 AM', isTakeAction: true}, 
-	                           {id: '', name: 'นัด Call', dateTimeDesc: 'Yesterday', isTakeAction: false}, 
-	                           {id: '', name: 'นัดชำระ', dateTimeDesc: 'Yesterday', isTakeAction: true}];
+	
+	$scope.notificationGroups = [{id: 1, name: 'นัดชำระ', isActive: true, alertNum: 1}, 
+	                             {id: 2, name: 'นัด Call', isActive: false}, 
+	                             {id: 3, name: 'ทั่วไป', isActive: false, alertNum: 5}];
+	
+	$scope.notificationList = [{id: '', subject: 'New Comment', detail: '0910217894ผู้ค้ำนิภาพรหญิงแจ้งว่าเป็นลุกสาวแต่อยากทราบว่าเจ้าหน้าที่ติดต่อเรื่องอะไร 0910217894ผู้ค้ำนิภาพรหญิงแจ้งว่าเป็นลุกสาวแต่อยากทราบว่าเจ้าหน้าที่ติดต่อเรื่องอะไร', dateTimeDesc: '4 minutes ago', isTakeAction: true}, 
+	                           {id: '', subject: 'Message Sent', detail: 'ค้นหาเบอร์เพิ่มเติม 2ไม่พบข้อมูล', dateTimeDesc: '12 minutes ago', isTakeAction: false}, 
+	                           {id: '', subject: 'New Task', detail: '0887313792 รับสายแจ้งว่าคนซื้อจะไปจ่ายให้ แอต่ไม่รู้วันไหน', dateTimeDesc: '27 minutes ago', isTakeAction: true}, 
+	                           {id: '', subject: 'New Followers', detail: '0925939632ผู้ซื้อฝากหมายเลขโทรกลับ/ไม่มีผู้ค้ำ', dateTimeDesc: '43 minutes ago', isTakeAction: true}, 
+	                           {id: '', subject: 'Server Rebooted', detail: '0895487336ผู้ซื้อฝากหมายเลขโทรกลับ/ไม่มีเบอร์ติดต่อผู้ค้ำจากหน้าระบบ', dateTimeDesc: '11:32 AM', isTakeAction: false}, 
+	                           {id: '', subject: 'Server Crashed!', detail: '0917645682 ผู้ซื้อคเชนทร์ พุกจีน ไม่สามารถติดต่อได้/0876887132 ผู้ค้ำ เรืองวิทย์ พงษ์แสวง ยังไม่เปิดใช้บริการ', dateTimeDesc: '11:13 AM', isTakeAction: false}, 
+	                           {id: '', subject: 'Server Not Responding', detail: '0937791924 ผู้ค้ำ ผู้หญิงรับสายแจ้งไม่มีไม่รู้จัก', dateTimeDesc: '10:57 AM', isTakeAction: true}, 
+	                           {id: '', subject: 'New Order Placed', detail: '062-3609484ติดต่อไม่ได้ 098-0233774 ไม่รับสาย', dateTimeDesc: '9:49 AM', isTakeAction: true}, 
+	                           {id: '', subject: 'นัด Call', detail: '034296451 ผู้เช้าซื้อ ติดต่อไม่ได้ 0861757391 ผู้ค้ำ ติดต่อไม่ได้', dateTimeDesc: 'Yesterday', isTakeAction: false}, 
+	                           {id: '', subject: 'นัดชำระ', detail: '0836945303ผู้ซื้อปิดเครื่อง/0834276267ผู้ค้ำศราวุธชายรับสายแจ้งผู้ค้ำไปทำงานเดี๋ยวให้ติดต่อกลับ', dateTimeDesc: 'Yesterday', isTakeAction: true}];
 	
 	
 	
