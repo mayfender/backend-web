26c1c9160e3074bca716111cc1a845d3a22672dc
sarawuti <sarawut.inthong@allianz.com>
2018-07-24 12:33:40 +0700

    update

--- backend/src/main/webapp/app/scripts/directives/header/header.js
+++ backend/src/main/webapp/app/scripts/directives/header/header.js
@@ -24,42 +24,7 @@ angular.module('sbAdminApp')
 	        		
 	        		$rootScope.workingOnProduct = product; 
 	        		$state.go('dashboard.home');
-	        		
-	        		
-	        		/*$http.post(urlPrefix + '/restAct/user/updateUserSetting', {
-	        			username: $localStorage.username,
-	        			currentProduct: id
-	        		}).then(function(data) {
-	        			if(data.data.statusCode != 9999) {				
-	        				$rootScope.systemAlert(data.data.statusCode);
-	        				return;
-	        			}
-	        			
-	        			if(!$rootScope.setting) $rootScope.setting = {};
-	        				
-	        			$scope.currentProduct = id;
-	        			$rootScope.setting.currentProduct = $scope.currentProduct;
-	        			
-	        			$state.go('dashboard.home');
-	        		}, function(response) {
-	        			$rootScope.systemAlert(response.status);
-	        		});*/
 	        	}
-	        	
-	        	/*if($rootScope.setting && $rootScope.setting.currentProduct) {
-	        		if($scope.productsSelect.length == 1) {
-		        		if($scope.productsSelect[0].id != $rootScope.setting.currentProduct) {
-		        			$scope.changeProduct($scope.productsSelect[0].id);
-		        			return;
-		        		}
-	        		}
-	        		
-	        		$scope.currentProduct = $rootScope.setting.currentProduct;
-	        	} else {
-	        		if($scope.productsSelect && $scope.productsSelect.length > 0) {
-	        			$scope.changeProduct($scope.productsSelect[0].id);	        			
-	        		}
-	        	}*/
 	        	
 	        	//----------------------: FlipClock :---------------------------------
 	        	var clock;
