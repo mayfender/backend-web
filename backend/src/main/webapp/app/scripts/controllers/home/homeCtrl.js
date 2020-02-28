005f112e7f5b81768e15528e7d94533d3c9c8b27
LENOVO <LENOVO@LAPTOP-P3E1O27F>
2020-02-28 12:18:36 +0700

    update

--- backend/src/main/webapp/app/scripts/controllers/home/homeCtrl.js
+++ backend/src/main/webapp/app/scripts/controllers/home/homeCtrl.js
@@ -1,4 +1,4 @@
-angular.module('sbAdminApp').controller('HomeCtrl', function($rootScope, $scope, $base64, $http, $translate, $localStorage, $state, $ngConfirm, urlPrefix) {
+angular.module('sbAdminApp').controller('HomeCtrl', function($rootScope, $scope, $base64, $http, $translate, $localStorage, $state, $timeout, $ngConfirm, urlPrefix) {
 	
 	if($rootScope.authority == 'ROLE_SUPERVISOR') {
 		$scope.position = 'Supervisor';
@@ -9,5 +9,15 @@ angular.module('sbAdminApp').controller('HomeCtrl', function($rootScope, $scope,
 	} else if ($rootScope.authority == 'ROLE_MANAGER') {
 		$scope.position = 'Manager';				
 	}
+	
+	
+	$scope.$watch('$viewContentLoaded', 
+    	function() {
+	        $timeout(function() {
+	        	//-- Set screen to display on mobile.
+	        	$('body').css('min-width', '1024px');    	        	
+	        }, 0);
+		}
+	);
 
 });
