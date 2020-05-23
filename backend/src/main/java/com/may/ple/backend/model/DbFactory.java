37d9c8949dee4b5e2e72c0c99b7655cf49362a21
LENOVO <LENOVO@LAPTOP-P3E1O27F>
2020-05-23 20:12:45 +0700

    update

---
+++ backend/src/main/java/com/may/ple/backend/model/DbFactory.java
@@ -0,0 +1,18 @@
+package com.may.ple.backend.model;
+
+import java.util.Map;
+
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+public class DbFactory {
+	private Map<String, MongoTemplate> templates;
+
+	public Map<String, MongoTemplate> getTemplates() {
+		return templates;
+	}
+
+	public void setTemplates(Map<String, MongoTemplate> templates) {
+		this.templates = templates;
+	}
+
+}
