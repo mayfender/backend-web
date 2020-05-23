81cb7c706d9c664c4da2e0ac75565398580b33d5
LENOVO <LENOVO@LAPTOP-P3E1O27F>
2020-05-23 20:25:14 +0700

    update

--- backend/src/main/java/com/may/ple/backend/criteria/ProfileUpdateCriteriaReq.java
+++ backend/src/main/java/com/may/ple/backend/criteria/ProfileUpdateCriteriaReq.java
@@ -1,6 +1,8 @@
 package com.may.ple.backend.criteria;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class ProfileUpdateCriteriaReq {
@@ -9,10 +11,25 @@ public class ProfileUpdateCriteriaReq {
 	private String newUserNameShow;
 	private String newUserName;
 	private String password;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String phoneExt;
+	private String imgContent;
+	private String imgName;
+	private Boolean isChangedImg;
+	private String title;
+	private List<String> productIds;
 	
 	@Override
 	public String toString() {
-		return ToStringBuilder.reflectionToString(this, ToStringStyle.DEFAULT_STYLE);
+		
+		ReflectionToStringBuilder stringBuilder = new ReflectionToStringBuilder(this, ToStringStyle.DEFAULT_STYLE);
+		stringBuilder.setAppendStatics(true);
+		stringBuilder.setAppendTransients(true);
+		stringBuilder.setExcludeFieldNames("imgContent");
+		
+		return stringBuilder.toString();
 	}
 
 	public String getOldUserName() {
@@ -53,6 +70,78 @@ public class ProfileUpdateCriteriaReq {
 
 	public void setNewUserNameShow(String newUserNameShow) {
 		this.newUserNameShow = newUserNameShow;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getImgContent() {
+		return imgContent;
+	}
+
+	public void setImgContent(String imgContent) {
+		this.imgContent = imgContent;
+	}
+
+	public String getImgName() {
+		return imgName;
+	}
+
+	public void setImgName(String imgName) {
+		this.imgName = imgName;
+	}
+
+	public Boolean getIsChangedImg() {
+		return isChangedImg;
+	}
+
+	public void setIsChangedImg(Boolean isChangedImg) {
+		this.isChangedImg = isChangedImg;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPhoneExt() {
+		return phoneExt;
+	}
+
+	public void setPhoneExt(String phoneExt) {
+		this.phoneExt = phoneExt;
+	}
+
+	public List<String> getProductIds() {
+		return productIds;
+	}
+
+	public void setProductIds(List<String> productIds) {
+		this.productIds = productIds;
 	}
 
 }
