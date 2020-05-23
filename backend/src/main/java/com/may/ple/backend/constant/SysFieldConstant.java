81006ff1cae288ca08fb0c9e1a271be812c3fcf9
LENOVO <LENOVO@LAPTOP-P3E1O27F>
2020-05-23 20:19:53 +0700

    update

---
+++ backend/src/main/java/com/may/ple/backend/constant/SysFieldConstant.java
@@ -0,0 +1,48 @@
+package com.may.ple.backend.constant;
+
+public enum SysFieldConstant {
+	SYS_OWNER("sys_owner"),
+	SYS_OWNER_FIRST_NAME("sys_owner_first_name"),
+	SYS_OWNER_LAST_NAME("sys_owner_last_name"),
+	SYS_OWNER_FULL_NAME("sys_owner_full_name"),
+	SYS_CREATED_FIRST_NAME("sys_created_first_name"),
+	SYS_CREATED_LAST_NAME("sys_created_last_name"),
+	SYS_CREATED_FULL_NAME("sys_created_full_name"),
+	SYS_OWNER_ID("sys_owner_id"),
+	SYS_PROBATION_OWNER_ID("sys_probation_owner_id"),
+	SYS_OLD_ORDER("sys_oldOrder"),
+	SYS_IS_ACTIVE("sys_isActive"),
+	SYS_FILE_ID("sys_fileId"),
+	SYS_APPOINT_DATE("sys_appointDate"),
+	SYS_APPOINT_AMOUNT("sys_appointAmount"),
+	SYS_NEXT_TIME_DATE("sys_nextTimeDate"),
+	SYS_TRACE_DATE("sys_traceDate"),
+	SYS_RESULT_TEXT("sys_resultText"),
+	SYS_COMPARE_DATE_STATUS("sys_compareDateStatus"),
+	SYS_CREATED_DATE_TIME("sys_createdDateTime"),
+	SYS_UPDATED_DATE_TIME("sys_updatedDateTime"),
+	SYS_TAGS("sys_tags"),
+	SYS_TAGS_U("sys_tags_u"),
+	SYS_NOW_DATETIME("sys_now_datetime"),
+	SYS_COUNT("sys_count");
+	
+	private String name;
+	
+	private SysFieldConstant(String name) {
+		this.name = name;
+	}
+	
+	public static SysFieldConstant findByName(String name) {
+		SysFieldConstant[] values = SysFieldConstant.values();
+		for (SysFieldConstant rolesConstant : values) {
+			if(rolesConstant.getName().equals(name)) 
+				return rolesConstant;
+		}
+		return null;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
