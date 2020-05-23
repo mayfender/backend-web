b8ff25edb5e35d919ada753eea76de053103e91f
LENOVO <LENOVO@LAPTOP-P3E1O27F>
2020-05-23 20:17:16 +0700

    update

--- backend/src/main/java/com/may/ple/backend/criteria/PersistUserCriteriaReq.java
+++ backend/src/main/java/com/may/ple/backend/criteria/PersistUserCriteriaReq.java
@@ -1,62 +1,154 @@
 package com.may.ple.backend.criteria;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class PersistUserCriteriaReq {
-	private long id;
-	private String userNameShow;
-	private String userName;
+	private String id;
+	private String showname;
+	private String username;
 	private String password;
 	private String authority;
-	private int status;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String phoneExt;
+	private String imgContent;
+	private String imgName;
+	private Boolean enabled;
+	private List<String> productIds;
+	private Boolean isChangedImg;
+	private String title;
+	private Boolean probation;
 	
 	@Override
 	public String toString() {
-		return ToStringBuilder.reflectionToString(this, ToStringStyle.DEFAULT_STYLE);
+		
+		ReflectionToStringBuilder stringBuilder = new ReflectionToStringBuilder(this, ToStringStyle.DEFAULT_STYLE);
+		stringBuilder.setAppendStatics(true);
+		stringBuilder.setAppendTransients(true);
+		stringBuilder.setExcludeFieldNames("imgContent");
+		
+		return stringBuilder.toString();
 	}
 	
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public int getStatus() {
-		return status;
-	}
-	public void setStatus(int status) {
-		this.status = status;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
 	public String getAuthority() {
 		return authority;
 	}
-
 	public void setAuthority(String authority) {
 		this.authority = authority;
 	}
-
-	public String getUserNameShow() {
-		return userNameShow;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Boolean getEnabled() {
+		return enabled;
+	}
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+	public String getShowname() {
+		return showname;
+	}
+	public void setShowname(String showname) {
+		this.showname = showname;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
-	public void setUserNameShow(String userNameShow) {
-		this.userNameShow = userNameShow;
+	public List<String> getProductIds() {
+		return productIds;
+	}
+
+	public void setProductIds(List<String> productIds) {
+		this.productIds = productIds;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getImgContent() {
+		return imgContent;
+	}
+
+	public void setImgContent(String imgContent) {
+		this.imgContent = imgContent;
+	}
+
+	public String getImgName() {
+		return imgName;
+	}
+
+	public void setImgName(String imgName) {
+		this.imgName = imgName;
+	}
+
+	public Boolean getIsChangedImg() {
+		return isChangedImg;
+	}
+
+	public void setIsChangedImg(Boolean isChangedImg) {
+		this.isChangedImg = isChangedImg;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPhoneExt() {
+		return phoneExt;
+	}
+
+	public void setPhoneExt(String phoneExt) {
+		this.phoneExt = phoneExt;
+	}
+
+	public Boolean getProbation() {
+		return probation;
+	}
+
+	public void setProbation(Boolean probation) {
+		this.probation = probation;
 	}
 	
 }
