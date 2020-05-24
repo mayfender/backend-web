a4e058b6405afb1df993442935da58566fdf9f84
LENOVO <LENOVO@LAPTOP-P3E1O27F>
2020-05-24 12:55:09 +0700

    update

--- backend/src/main/webapp/app/scripts/controllers/order/orderCtrl.js
+++ backend/src/main/webapp/app/scripts/controllers/order/orderCtrl.js
@@ -1,6 +1,36 @@
-angular.module('sbAdminApp').controller('OrderCtrl', function($rootScope, $scope, $base64, $http, $translate, urlPrefix, loadData) {
+angular.module('sbAdminApp').controller('OrderCtrl', function($rootScope, $scope, $base64, $http, $translate, $ngConfirm, urlPrefix, loadData) {
 	console.log('Order');
 	
+	$scope.periods = [{id: '1', name: '01/01/2020'},{id: '2', name: '15/01/2020'},{id: '3', name: '01/02/2020'}];
+	$scope.formData = {period: $scope.periods[0].id};
+	
+	$scope.mayfender = 'may';
+	
+	$scope.addPeriod = function() {
+		$ngConfirm({
+		    title: 'เพิ่มงวดใหม่',
+		    contentUrl: './views/order/addPeriod.html',
+		    type: 'blue',
+		    typeAnimated: true,
+		    scope: $scope,
+		    columnClass: 'col-xs-8 col-xs-offset-2',
+		    buttons: {
+		        save: {
+		            text: 'บันทึก',
+		            btnClass: 'btn-blue',
+		            action: function(){
+		            	
+		            }
+		        },
+		        close: {
+		        	text: 'ยกเลิก',
+		        	action: function(){
+		            	
+		            }
+		        }
+		    }
+		});
+	}
 	
 	
 });
