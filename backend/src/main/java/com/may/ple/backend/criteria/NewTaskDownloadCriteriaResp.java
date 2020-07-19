3235a880d312429dce95815a0bd0fc389dcebc57
LENOVO <LENOVO@LAPTOP-P3E1O27F>
2020-07-19 18:54:47 +0700

    update

--- backend/src/main/java/com/may/ple/backend/criteria/NewTaskDownloadCriteriaResp.java
+++ backend/src/main/java/com/may/ple/backend/criteria/NewTaskDownloadCriteriaResp.java
@@ -44,7 +44,7 @@ public class NewTaskDownloadCriteriaResp extends CommonCriteriaResp implements S
 	private DymListService dymService;
 	private ExcelReport excelUtil;
 	private String contractNoColumnName;
-	
+
 	@Override
 	public void write(OutputStream os) throws IOException, WebApplicationException {
 		OutputStream out = null;
@@ -52,22 +52,22 @@ public class NewTaskDownloadCriteriaResp extends CommonCriteriaResp implements S
 		FileInputStream fis = null;
 		XSSFWorkbook workbook = null;
 		Date maxDate = new Date(Long.MAX_VALUE);
-		
+
 		try {
 			byte[] data;
 			out = new BufferedOutputStream(os);
-			
+
 			if(isCheckData) {
 				if(filePath.endsWith(".xlsx")) {
 					workbook = new XSSFWorkbook(new FileInputStream(filePath));
 				} else {
 					throw new CustomerException(5000, "Filetype not match");
 				}
-				
+
 				if(!isByCriteria) workbook.setSheetName(0, workbook.getSheetName(0) + "_Validation");
-				
+
 				XSSFSheet sheet = workbook.getSheetAt(0);
-				
+
 				if(FilenameUtils.getBaseName(filePath).contains("_newversion")) {
 					LOG.info("Use new template");
 					//--[.]
@@ -75,21 +75,23 @@ public class NewTaskDownloadCriteriaResp extends CommonCriteriaResp implements S
 					Set<String> keySet = holderResp.fields.keySet();
 					List<String> fields = new ArrayList<>(keySet);
 					List<String> codeFields = new ArrayList<>();
-					
+
 					if(!fields.contains("user")) {
 						fields.add("user");
 					}
-					
+
+					String[] keys = null;
 					for (int i = 0; i < fields.size(); i++) {
-						if(fields.get(i).endsWith("_sys")) {
+						if(fields.get(i).startsWith("link_")) {
 							codeFields.add(fields.get(i));
-							fields.set(i, fields.get(i).replace("_sys", ""));
+							keys = fields.get(i).replace("link_", "").split("\\.");
+							fields.set(i, keys[0]);
 						}
 					}
-					
+
 					List<Map> taskDetails = service.find(req, fields).getTaskDetails();
 					extraData(taskDetails, keySet, fields, codeFields);
-					
+
 					excelUtil.fillBody(
 							holderResp,
 							sheet,
@@ -104,13 +106,13 @@ public class NewTaskDownloadCriteriaResp extends CommonCriteriaResp implements S
 					List<ColumnFormat> columnFormats = new ArrayList<>();
 					Map<String, Integer> headerIndex = GetAccountListHeaderUtil.getFileHeader(sheet, columnFormats);
 					Set<String> keySet = headerIndex.keySet();
-					
+
 					List<String> fields = new ArrayList<>(keySet);
 					List<String> codeFields = new ArrayList<>();
 					Map<String, List<Map<String, String>>> dynResult = new HashMap<>();
 					List<Map<String, String>> result;
-					
-					if(fields != null) {					
+
+					if(fields != null) {
 						for (int i = 0; i < fields.size(); i++) {
 							if(fields.get(i).endsWith("_sys")) {
 								codeFields.add(fields.get(i));
@@ -136,35 +138,35 @@ public class NewTaskDownloadCriteriaResp extends CommonCriteriaResp implements S
 							}
 						}
 					}
-					
+
 					List<Map> taskDetails = service.find(req, fields).getTaskDetails();
-					
+
 					CellCopyPolicy cellCopyPolicy = new CellCopyPolicy();
 					cellCopyPolicy.setCopyCellStyle(true);
 					int rowIndex = 1;
 					Row row;
 					Cell cell;
 					Object val;
-					
-					for (Map task : taskDetails) {			
-						
+
+					for (Map task : taskDetails) {
+
 						row = sheet.getRow(rowIndex++);
-						
+
 						if(rowIndex != 1) {
-							sheet.copyRows(1, 1, rowIndex, cellCopyPolicy);	
+							sheet.copyRows(1, 1, rowIndex, cellCopyPolicy);
 						}
-						
+
 						/*if(row == null) {
 						row = sheet.createRow(rowIndex - 1);
 					}*/
-						
+
 						for (String key : keySet) {
 							if(dynResult.containsKey(key)) {
 								val = task.get(key.replace("_sys", ""));
-								
+
 								if(val != null) {
 									result = dynResult.get(key);
-									
+
 									for (Map<String, String> map : result) {
 										if(val.equals(map.get("_id"))) {
 											if(map.containsKey("meaning") && !StringUtils.isBlank(StringUtils.stripToEmpty(map.get("meaning")))) {
@@ -182,28 +184,28 @@ public class NewTaskDownloadCriteriaResp extends CommonCriteriaResp implements S
 							} else {
 								val = task.get(key);
 							}
-							
+
 							cell = row.getCell(headerIndex.get(key));
-							
+
 							if(val == null) {
 								if(cell != null) {
 									cell.setCellType(Cell.CELL_TYPE_BLANK);
 								}
 								continue;
 							}
-							
+
 							/*if(cell == null) {
 							cell = row.createCell(headerIndex.get(key));
 							cell.setCellStyle(cellStyleMap.get(headerIndex.get(key)));
 						} else {
 							if(cellStyleMap.get(headerIndex.get(key)) == null) {
-								cellStyleMap.put(headerIndex.get(key), cell.getCellStyle());								
+								cellStyleMap.put(headerIndex.get(key), cell.getCellStyle());
 							}
 						}*/
-							
+
 							if(val instanceof Date) {
 								if(((Date) val).compareTo(maxDate) != 0) {
-									cell.setCellValue((Date)val);									
+									cell.setCellValue((Date)val);
 								} else {
 									cell.setCellValue("");
 								}
@@ -216,15 +218,15 @@ public class NewTaskDownloadCriteriaResp extends CommonCriteriaResp implements S
 							}
 						}
 					}
-					
+
 					if(isByCriteria) {
 						int countNull = 0;
-						
+
 						while(true) {
 							if(countNull == 10) break;
-							
+
 							row  = sheet.getRow(rowIndex++);
-							
+
 							if(row != null) {
 								sheet.removeRow(row);
 							} else {
@@ -233,38 +235,38 @@ public class NewTaskDownloadCriteriaResp extends CommonCriteriaResp implements S
 						}
 					}
 				}
-				
+
 				workbook.write(out);
 			} else {
 				LOG.debug("Get byte");
 				java.nio.file.Path path = Paths.get(filePath);
-				data = Files.readAllBytes(path);					
-			
+				data = Files.readAllBytes(path);
+
 				in = new ByteArrayInputStream(data);
 				int bytes;
-				
+
 				while ((bytes = in.read()) != -1) {
 					out.write(bytes);
 				}
 			}
-			
+
 			LOG.debug("End");
 		} catch (Exception e) {
 			LOG.error(e.toString());
 		} finally {
 			try { if(workbook != null) workbook.close(); } catch (Exception e2) {}
 			try { if(fis != null) fis.close(); } catch (Exception e2) {}
-			try { if(in != null) in.close(); } catch (Exception e2) {}			
-			try { if(out != null) out.close(); } catch (Exception e2) {}			
-		}	
+			try { if(in != null) in.close(); } catch (Exception e2) {}
+			try { if(out != null) out.close(); } catch (Exception e2) {}
+		}
 	}
-	
+
 	private void extraData(List<Map> taskDetails, Set<String> keySet, List<String> fields, List<String> codeFields) throws Exception {
 		try {
 			Map<String, List<Map<String, String>>> dynResult = new HashMap<>();
 			List<Map<String, String>> result;
 			Object val;
-			
+
 			if(codeFields.size() > 0) {
 				DymListFindCriteriaReq reqDym = new DymListFindCriteriaReq();
 				List<Integer> statuses = new ArrayList<>();
@@ -277,40 +279,40 @@ public class NewTaskDownloadCriteriaResp extends CommonCriteriaResp implements S
 					for (Map parent : dynListFull) {
 						if(parent.get("fieldName").equals(field)) {
 							result = (List<Map<String, String>>)parent.get("dymListDet");
-							dynResult.put(field + "_sys", result);
+							dynResult.put("link_" + field, result);
 							break;
 						}
 					}
 				}
 			}
-			
-			for (Map task : taskDetails) {			
+
+			String keys[] = null;
+			for (Map task : taskDetails) {
 				for (String key : keySet) {
-					
-					if(!dynResult.containsKey(key)) continue;
-					
-					val = task.get(key.replace("_sys", ""));
-					
+					if(!key.startsWith("link_")) continue;
+
+					keys = key.split("\\.");
+
+					if(!dynResult.containsKey(keys[0])) continue;
+
+					val = task.get(keys[0].replace("link_", ""));
+
 					if(val == null) continue;
-					
-					result = dynResult.get(key);
-						
+
+					result = dynResult.get(keys[0]);
+
 					for (Map<String, String> map : result) {
 						if(val.equals(map.get("_id"))) {
-							if(map.containsKey("code") && !StringUtils.isBlank(StringUtils.stripToEmpty(map.get("code")))) {
+							if(keys[1].equals("code") && !StringUtils.isBlank(StringUtils.stripToEmpty(map.get("code")))) {
 								val = StringUtils.stripToEmpty(map.get("code"));
-							} else {								
-								if(map.containsKey("meaning") && !StringUtils.isBlank(StringUtils.stripToEmpty(map.get("meaning")))) {
-									val = StringUtils.stripToEmpty(map.get("meaning"));
-								} else {
-									val = "";
-								}
+							} else if(keys[1].equals("meaning") && !StringUtils.isBlank(StringUtils.stripToEmpty(map.get("meaning")))) {
+								val = StringUtils.stripToEmpty(map.get("meaning"));
+							} else {
+								val = "";
 							}
-							
 							break;
 						}
 					}
-					
 					task.put(key, val);
 				}
 			}
@@ -319,7 +321,7 @@ public class NewTaskDownloadCriteriaResp extends CommonCriteriaResp implements S
 			throw e;
 		}
 	}
-	
+
 	public String getFilePath() {
 		return filePath;
 	}
