74d430eeb828f97446d1b564dc499e18c038efd8
LENOVO <LENOVO@LAPTOP-P3E1O27F>
2020-08-08 07:11:51 +0700

    update

--- backend/src/main/java/com/may/ple/backend/service/ReceiverService.java
+++ backend/src/main/java/com/may/ple/backend/service/ReceiverService.java
@@ -80,40 +80,6 @@ public class ReceiverService {
 			rv.setReceiverName(req.getReceiverName());
 			rv.setSenderName(req.getSenderName());
 
-			// Pass
-			rv.setPassPriceBon3(req.getPassPriceBon3());
-			rv.setPassPriceBon2(req.getPassPriceBon2());
-			rv.setPassPriceLang2(req.getPassPriceLang2());
-			rv.setPassPriceTod(req.getPassPriceTod());
-			rv.setPassPriceLoy1(req.getPassPriceLoy1());
-			rv.setPassPriceLoy4(req.getPassPriceLoy4());
-			rv.setPassPriceLoy5(req.getPassPriceLoy5());
-
-			rv.setPassPerBon3(req.getPassPerBon3());
-			rv.setPassPerBon2(req.getPassPerBon2());
-			rv.setPassPerLang2(req.getPassPerLang2());
-			rv.setPassPerTod(req.getPassPerTod());
-			rv.setPassPerLoy1(req.getPassPerLoy1());
-			rv.setPassPerLoy4(req.getPassPerLoy4());
-			rv.setPassPerLoy5(req.getPassPerLoy5());
-
-			// Sale
-			rv.setSalePriceBon3(req.getSalePriceBon3());
-			rv.setSalePriceBon2(req.getSalePriceBon2());
-			rv.setSalePriceLang2(req.getSalePriceLang2());
-			rv.setSalePriceTod(req.getSalePriceTod());
-			rv.setSalePriceLoy1(req.getSalePriceLoy1());
-			rv.setSalePriceLoy4(req.getSalePriceLoy4());
-			rv.setSalePriceLoy5(req.getSalePriceLoy5());
-
-			rv.setSalePerBon3(req.getSalePerBon3());
-			rv.setSalePerBon2(req.getSalePerBon2());
-			rv.setSalePerLang2(req.getSalePerLang2());
-			rv.setSalePerTod(req.getSalePerTod());
-			rv.setSalePerLoy1(req.getSalePerLoy1());
-			rv.setSalePerLoy4(req.getSalePerLoy4());
-			rv.setSalePerLoy5(req.getSalePerLoy5());
-
 			dealerTemp.save(rv);
 		} catch (Exception e) {
 			LOG.error(e.toString());
--- backend/src/main/webapp/app/scripts/controllers/receiver/receiverCtrl.js
+++ backend/src/main/webapp/app/scripts/controllers/receiver/receiverCtrl.js
@@ -8,16 +8,7 @@ angular.module('sbAdminApp').controller('ReceiverCtrl', function($rootScope, $st
 	};
 	$scope.receiver.formData = {};
 	$scope.receiver.list = loadData.receiverList;
-	
-	$scope.receiver.label = [{
-		group1: 'ราคา 3 / 2 / 2 ตัวล่าง',
-		group2: 'ราคา ลอย 1 ตัว / 4 ตัว / 5 ตัว',
-		group3: 'ราคา 3 ตัวโต๊ด',
-	}, {
-		group1: 'เปอร์เซ็น 3 / 2 / 2 ตัวล่าง',
-		group2: 'เปอร์เซ็น ลอย 1 ตัว / 4 ตัว / 5 ตัว',		
-		group3: 'เปอร์เซ็น 3 ตัวโต๊ด',
-	}];
+	$scope.priceList = {};
 	
 	$scope.statusToggle = function(e, obj) {
 		e.stopPropagation()
@@ -47,38 +38,7 @@ angular.module('sbAdminApp').controller('ReceiverCtrl', function($rootScope, $st
 			$scope.receiver.formData.receiverId = obj.id;
 			$scope.receiver.formData.senderName = obj.senderName;
 			$scope.receiver.formData.receiverName = obj.receiverName;
-			
-			$scope.receiver.pass[0].fieldBon3 = obj.passPriceBon3;
-			$scope.receiver.pass[0].fieldBon2 = obj.passPriceBon2;
-			$scope.receiver.pass[0].fieldLang2 = obj.passPriceLang2;
-			$scope.receiver.pass[0].fieldTod = obj.passPriceTod;
-			$scope.receiver.pass[0].fieldLoy1 = obj.passPriceLoy1;
-			$scope.receiver.pass[0].fieldLoy4 = obj.passPriceLoy4;
-			$scope.receiver.pass[0].fieldLoy5 = obj.passPriceLoy5;
-			
-			$scope.receiver.pass[1].fieldBon3 = obj.passPerBon3;
-			$scope.receiver.pass[1].fieldBon2 = obj.passPerBon2;
-			$scope.receiver.pass[1].fieldLang2 = obj.passPerLang2;
-			$scope.receiver.pass[1].fieldTod = obj.passPerTod;
-			$scope.receiver.pass[1].fieldLoy1 = obj.passPerLoy1;
-			$scope.receiver.pass[1].fieldLoy4 = obj.passPerLoy4;
-			$scope.receiver.pass[1].fieldLoy5 = obj.passPerLoy5;
-			
-			$scope.receiver.sale[0].fieldBon3 = obj.salePriceBon3;
-			$scope.receiver.sale[0].fieldBon2 = obj.salePriceBon2;
-			$scope.receiver.sale[0].fieldLang2 = obj.salePriceLang2;
-			$scope.receiver.sale[0].fieldTod = obj.salePriceTod;
-			$scope.receiver.sale[0].fieldLoy1 = obj.salePriceLoy1;
-			$scope.receiver.sale[0].fieldLoy4 = obj.salePriceLoy4;
-			$scope.receiver.sale[0].fieldLoy5 = obj.salePriceLoy5;
-			
-			$scope.receiver.sale[1].fieldBon3 = obj.salePerBon3;
-			$scope.receiver.sale[1].fieldBon2 = obj.salePerBon2;
-			$scope.receiver.sale[1].fieldLang2 = obj.salePerLang2;
-			$scope.receiver.sale[1].fieldTod = obj.salePerTod;
-			$scope.receiver.sale[1].fieldLoy1 = obj.salePerLoy1;
-			$scope.receiver.sale[1].fieldLoy4 = obj.salePerLoy4;
-			$scope.receiver.sale[1].fieldLoy5 = obj.salePerLoy5;
+			$scope.receiver.formData.priceListId = obj.priceListId;
 		} else {
 			initFields();
 		}
@@ -97,42 +57,7 @@ angular.module('sbAdminApp').controller('ReceiverCtrl', function($rootScope, $st
 			receiverName: $scope.receiver.formData.receiverName,
 			senderName: $scope.receiver.formData.senderName,
 			dealerId: $rootScope.workingOnDealer.id,
-			
-			id: $scope.receiver.formData.receiverId,
-			
-			//Pass
-			passPriceBon3: $scope.receiver.pass[0].fieldBon3,
-			passPriceBon2: $scope.receiver.pass[0].fieldBon2,
-			passPriceLang2: $scope.receiver.pass[0].fieldLang2,
-			passPriceTod: $scope.receiver.pass[0].fieldTod,
-			passPriceLoy1: $scope.receiver.pass[0].fieldLoy1,
-			passPriceLoy4: $scope.receiver.pass[0].fieldLoy4,
-			passPriceLoy5: $scope.receiver.pass[0].fieldLoy5,
-			
-			passPerBon3: $scope.receiver.pass[1].fieldBon3,
-			passPerBon2: $scope.receiver.pass[1].fieldBon2,
-			passPerLang2: $scope.receiver.pass[1].fieldLang2,
-			passPerTod: $scope.receiver.pass[1].fieldTod,
-			passPerLoy1: $scope.receiver.pass[1].fieldLoy1,
-			passPerLoy4: $scope.receiver.pass[1].fieldLoy4,
-			passPerLoy5: $scope.receiver.pass[1].fieldLoy5,
-			
-			//Sale
-			salePriceBon3: $scope.receiver.sale[0].fieldBon3,
-			salePriceBon2: $scope.receiver.sale[0].fieldBon2,
-			salePriceLang2: $scope.receiver.sale[0].fieldLang2,
-			salePriceTod: $scope.receiver.sale[0].fieldTod,
-			salePriceLoy1: $scope.receiver.sale[0].fieldLoy1,
-			salePriceLoy4: $scope.receiver.sale[0].fieldLoy4,
-			salePriceLoy5: $scope.receiver.sale[0].fieldLoy5,
-			
-			salePerBon3: $scope.receiver.sale[1].fieldBon3,
-			salePerBon2: $scope.receiver.sale[1].fieldBon2,
-			salePerLang2: $scope.receiver.sale[1].fieldLang2,
-			salePerTod: $scope.receiver.sale[1].fieldTod,
-			salePerLoy1: $scope.receiver.sale[1].fieldLoy1,
-			salePerLoy4: $scope.receiver.sale[1].fieldLoy4,
-			salePerLoy5: $scope.receiver.sale[1].fieldLoy5
+			id: $scope.receiver.formData.receiverId
 		}).then(function(data) {
 			var result = data.data;
 			if(result.statusCode != 9999) {
@@ -157,6 +82,21 @@ angular.module('sbAdminApp').controller('ReceiverCtrl', function($rootScope, $st
 			}
 			
 			$scope.receiver.list = result.receiverList;
+		}, function(response) {
+			$rootScope.systemAlert(response.status);
+		});
+	}
+	
+	function getPriceList() {
+		$http.get(urlPrefix + '/restAct/receiver/getPriceList?dealerId=' + $rootScope.workingOnDealer.id + '&enabled=true').then(function(data){
+			var result = data.data;
+			if(result.statusCode != 9999) {
+				$rootScope.systemAlert(result.statusCode);
+				return;
+			}
+			
+			$scope.priceList.list = result.priceList;
+			console.log($scope.priceList.list);
 		}, function(response) {
 			$rootScope.systemAlert(response.status);
 		});
@@ -200,47 +140,10 @@ angular.module('sbAdminApp').controller('ReceiverCtrl', function($rootScope, $st
 		$scope.receiver.formData.receiverId = null;
 		$scope.receiver.formData.senderName = null;
 		$scope.receiver.formData.receiverName = null;
-		
-		//-- ส่ง
-		$scope.receiver.pass = [{
-			fieldBon3: null,
-			fieldBon2: null,
-			fieldLang2: null,
-			fieldTod: null,
-			fieldLoy1: null,
-			fieldLoy4: null,
-			fieldLoy5: null
-		},{
-			fieldBon3: null,
-			fieldBon2: null,
-			fieldLang2: null,
-			fieldTod: null,
-			fieldLoy1: null,
-			fieldLoy4: null,
-			fieldLoy5: null
-		}];
-		
-		//-- ขาย
-		$scope.receiver.sale = [{
-			fieldBon3: null,
-			fieldBon2: null,
-			fieldLang2: null,
-			fieldTod: null,
-			fieldLoy1: null,
-			fieldLoy4: null,
-			fieldLoy5: null
-		},{
-			fieldBon3: null,
-			fieldBon2: null,
-			fieldLang2: null,
-			fieldTod: null,
-			fieldLoy1: null,
-			fieldLoy4: null,
-			fieldLoy5: null
-		}];
 	}
 	
 	initFields();
+	getPriceList();
 	
 	
 	$scope.$watch('$viewContentLoaded', 
